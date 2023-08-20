bfcbf23805f0210e963d3ea5320a85ba21282124
HubCJ <u20201b742@upc.edu.pe>
2023-08-20 11:12:49 -0500

    create project

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'pay-car',
+    appId: '1:55215772622:web:9fc73ed0222bf23ffc119f',
+    storageBucket: 'pay-car.appspot.com',
+    apiKey: 'AIzaSyAw9lY_z5qn4QLlG-i_GvSIbtDqyJ_Mw7s',
+    authDomain: 'pay-car.firebaseapp.com',
+    messagingSenderId: '55215772622',
+  },};
