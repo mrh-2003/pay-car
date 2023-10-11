12a73398586369563e86795eb36c7c5ee0c667e4
hubcj2003 <87394635+hubcj2003@users.noreply.github.com>
2023-10-11 00:59:42 +0000

    hola

---
+++ src/app/models/corrida.ts
@@ -0,0 +1,12 @@
+export class Corrida {
+    id : string = ""; 
+    moneda: string = "";
+    valorVehiculo : number = 0; 
+    cuotaInicial: number = 0; 
+    cuotaFinal: number = 0; 
+    valorTasa: number = 0; 
+    frecuenciaPago: string = "";
+    plazoAnios: number = 0; 
+    COK: number = 0; 
+    idUsuario: string = ""; 
+}
---
+++ src/app/models/usuario.ts
@@ -0,0 +1,7 @@
+export class Usuario{
+    id : string = ""; 
+    nombre: string = ""; 
+    apellido: string = "";
+    email: string = "";
+    password: string = "";
+}
